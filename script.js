7096a65c938884e2bcf59990ef6fca2d3adf3ee1
Rocky <rrsalerno21@gmail.com>
2020-04-11 22:17:24 -0700

    joined pass array, cleaned code, added cancelled console log

--- script.js
+++ script.js
@@ -9,13 +9,9 @@ var generateBtn = document.querySelector("#generate");
 // Create an object that contains all of the arrays we'd like to utilize for password characters
 
 var charValues = {
-	
 	lowercase: ['a', 'b', 'c', 'd', 'e' , 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-
 	uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-
 	numeric: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-
 	specialChar: [` `, `!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `]`, `^`, `_`, `{`, `|`, `}`, `~`]
 }
 
@@ -44,36 +40,47 @@ function getPossChar(lower, upper, num, spec) {
 
 // Function to prompt user and store results in a returned object
 var getInputs = function () {
-	var passLength, lowCase, upCase, nums, specials;
+	var passLength, lowCase, upCase, nums, specials, inRange, atLeastOne, cancelled;
 
-	// * Use a while loop with a true/false flag to make sure the user selects a number within the correct range
-	var inRange = false;
+	// * Use a while loop with a true/false flag to make sure the user selects a number within the correct range and at least one character parameter
+	inRange = false;
 	while (!inRange) {
-		passLength = prompt(`How many characters would you like your password to have? (Must pick a number between 8 and 128.  Our default length is 10)`);
-		if ((parseInt(passLength) >= 8) && (parseInt(passLength) <= 128)) {
-			inRange = true;
-			console.log(`Password length set to: ${passLength}`)
-		} else if (passLength === null) {
+		// Password Length Prompt
+		passLength = prompt(`How many characters would you like your password to have? (Must pick a number between 8 and 128.  Our default length is 10.  Hit cancel to terminate password generation.)`);
+		
+		// Input cases
+		if (passLength === null) {
+			cancelled = true;
+			break;
+		} else if (passLength === "") {
 			passLength = 10;
 			inRange = true;
+		}		
+		else if ((parseInt(passLength) >= 8) && (parseInt(passLength) <= 128)) {
+			inRange = true;
+			console.log(`Password length set to: ${passLength}`)
 		} else {
 			alert(`Sorry, that number is not within the range of 8 - 128.  Try again.`);
-
 		}
 	}
 
 	// * Use a while loop with a true/false flag to make sure the user selects at least one character parameter
-	var atLeastOne = false;
+	atLeastOne = false;
 	while (!atLeastOne) {
-		lowCase = confirm(`Would you like to use lowercase characters in your password?`);
-		upCase = confirm(`Would you like to use uppercase characters in your password?`);
-		nums = confirm(`Would you like to use numbers in your password?`);
-		specials = confirm(`Would you like to use special characters in your password?`);
-
-		if (lowCase || upCase || nums || specials) {
-			atLeastOne = true;
+		if (cancelled) {
+			break
 		} else {
-			alert(`Sorry, you have to pick at least one character type to use!  Let's take it from the top again...`)
+			// Confirm parameter messages
+			lowCase = confirm(`Would you like to use lowercase characters in your password?`);
+			upCase = confirm(`Would you like to use uppercase characters in your password?`);
+			nums = confirm(`Would you like to use numbers in your password?`);
+			specials = confirm(`Would you like to use special characters in your password?`);
+	
+			if (lowCase || upCase || nums || specials) {
+				atLeastOne = true;
+			} else {
+				alert(`Sorry, you have to pick at least one character type to use!  Let's take it from the top again...`)
+			}
 		}
 	}
 
@@ -83,16 +90,17 @@ var getInputs = function () {
 		lowCase: lowCase,
 		upCase: upCase,
 		nums: nums,
-		specials: specials
+		specials: specials,
+		cancelled: cancelled
 	}
-
 
 	return {
 		passLength: inputs.passLength,
 		lowCase: inputs.lowCase,
 		upCase:  inputs.upCase,
 		nums: inputs.nums,
-		specials: inputs.specials
+		specials: inputs.specials,
+		cancelled: inputs.cancelled
 	}
 }
 
@@ -104,29 +112,30 @@ function generatePassword() {
 	
 	// 1. Prompt/confirm the user for password parameters and get inputs
 	inputs = getInputs();
-	console.log(`Successfully brought inputs into local scope: `, inputs);
-
-	// 2. If confirms are true, then add their respective arrays into possChar array as individual elements
-	possCharArray = getPossChar(inputs.lowCase, inputs.upCase, inputs.nums, inputs.specials);
-
-
-	// 3. Run a for loop to iterate over the length of the password
-	for (var i = 1; i <= inputs.passLength; i++) {
-
-		//4. Generate a random number as the index to grab the element from the array
-		ranNum = Math.floor(Math.random() * possCharArray.length);
-		console.log(ranNum);
-
-		pass.push(possCharArray[ranNum]);
-		console.log(pass);
-
-		//5.  Push that element into the pass array
-
-	}
-
+	if (inputs.cancelled) {
+		console.log(`User cancelled generate password.`);
+	} else {
+		console.log(`Successfully brought inputs into local scope of generatePassword: `, inputs);
 	
-	//6. Returned the joined array as a string
-
+		// 2. If confirms are true, then add their respective arrays into possChar array as individual elements
+		possCharArray = getPossChar(inputs.lowCase, inputs.upCase, inputs.nums, inputs.specials);
+	
+	
+		// 3. Run a for loop to iterate over the length of the password
+		for (var i = 1; i <= inputs.passLength; i++) {
+	
+			//4. Generate a random number as the index to grab the element from the array
+			ranNum = Math.floor(Math.random() * possCharArray.length);
+	
+			//5.  Push that element into the pass array
+			pass.push(possCharArray[ranNum]);
+		}
+	
+		console.log(`Password Array: `, pass);
+	
+		//6. Returned the joined array as a string
+		return pass.join("");
+	}
 }
 
 
